4f3304a1e3cd03aa470a68d583f380031d33dae1
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:43:47 +0100

    Create run-scummvm.sh

---
+++ .amibian_scripts/run-scummvm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#runs scummvm virtual machine.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# change to working directory
+cd /home/amibian/ScummVM/scummvm
+
+# start program
+./scummvm
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
