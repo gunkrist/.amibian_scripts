30c9b97b5fcba438572be5019382c114a78222eb
gunkrist <gunkrist79@gmail.com>
2020-01-13 22:15:30 +0100

    Create run-amiberry-main.sh

---
+++ .amibian_scripts/run-amiberry-main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#runs amiberry amiga emulator main branch.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+#clear the screen
+clear
+
+# change to working directory
+cd /home/amibian/Amiga/Emulators/amiberry
+
+# start program
+./amiberry -f /home/amibian/Amiga/Emulators/amiberry/conf/autostart.uae
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
