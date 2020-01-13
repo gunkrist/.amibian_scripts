f8f56581727f86f75c6f588281df8675e238d413
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:51:05 +0100

    Create run-desktop.sh

---
+++ .amibian_scripts/run-desktop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Starts the Amibian linux environment.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# start program
+startx
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
