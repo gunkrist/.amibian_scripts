aa7e78abd392539d08eb76845a81863ba3bee7bc
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:52:24 +0100

    Create run-c64.sh

---
+++ .amibian_scripts/run-c64.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#runs the c64 emulator from vice emulator.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+#clear the screen
+clear
+
+# change to working directory
+cd /home/amibian/Commodore
+
+# start program
+x64
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
