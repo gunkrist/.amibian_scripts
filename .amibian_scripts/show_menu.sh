8f062b62b90da7036836b62b2808d8cbe4912d20
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:32:53 +0100

    Create show_menu.sh

---
+++ .amibian_scripts/show_menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#displays the amibian cli menu.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# display the menu
+cat /home/amibian/.amibian_scripts/cli_menu/copyright.txt
+cat /home/amibian/.amibian_scripts/cli_menu/header.txt
+cat /home/amibian/.amibian_scripts/cli_menu/version.txt
+cat /home/amibian/.amibian_scripts/cli_menu/menu.txt
+cat /home/amibian/.amibian_scripts/cli_menu/promotion.txt
+cd ~
+# display cpu temp
+vcgencmd measure_temp
