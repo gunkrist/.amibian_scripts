09dfaacf6d651d06af929916fb6bc0f7537c4b8f
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:44:38 +0100

    Create run_midnight_commander.sh

---
+++ .amibian_scripts/run_midnight_commander.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#runs midnight commander file manager.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+cd ~
+
+sudo mc
+
+#clear screen on exit
+clear
+
+#show menu
+menu
