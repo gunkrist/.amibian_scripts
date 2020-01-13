1af4a0fe8aa4eae18a72970f4c98778a88b523a5
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:34:09 +0100

    Create show_instructions.sh

---
+++ .amibian_scripts/show_instructions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#show the help file
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# show the help files
+cat /home/amibian/.amibian_scripts/cli_menu/copyright.txt
+cat /home/amibian/.amibian_scripts/cli_menu/header.txt
+cat /home/amibian/.amibian_scripts/cli_menu/version.txt
+less /home/amibian/.amibian_scripts/instructions.txt
+
+#clear the screen
+clear
+
+#show menu
+menu
