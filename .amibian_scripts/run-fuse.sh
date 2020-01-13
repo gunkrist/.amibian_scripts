6116bca38d138959d165747c159b394547af2ce2
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:49:53 +0100

    Create run-fuse.sh

---
+++ .amibian_scripts/run-fuse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#runs fuse ZX Spectrum emulator.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# change to working directory
+cd /home/amibian/ZxSpectrum
+
+# start program
+clear
+fuse --kempston
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
