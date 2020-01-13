3aa1a5bfe4c105be2e1b69eea4c049a9a5ce59f6
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:31:47 +0100

    Create start-on-boot.sh

---
+++ .amibian_scripts/start-on-boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#runs an emulator or starts the amibian linux environment on boot.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# change to working directory
+cd /home/amibian/.amibian_scripts
+
+# run an emulator - this line will be edited by another script to select the emulator to boot
+./run-amiga.sh
+
+# change to home directory
+cd ~
+
+#clear screen on exit and show cli menu
+clear
+menu
+
