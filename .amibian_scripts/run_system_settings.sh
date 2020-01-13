0c629de4a3396e3fc0145a83b665a35e76723f66
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:42:34 +0100

    Create run_system_settings.sh

---
+++ .amibian_scripts/run_system_settings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#runs System setup.
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+cd ~
+
+sudo raspi-config
+
+#clear screen on exit
+clear
+
+#show menu
+menu
