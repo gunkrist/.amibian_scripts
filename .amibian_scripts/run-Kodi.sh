5fc6760ea610dc5e3909762be67010cbcb828327
gunkrist <gunkrist79@gmail.com>
2020-01-13 20:48:37 +0100

    Create run-Kodi.sh

---
+++ .amibian_scripts/run-Kodi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#runs kodi media player
+# Copyright © 2019 Amibian.net
+# All rights reserved
+
+# clear the screen
+clear
+
+# start program
+exec kodi
+
+# change to home directory
+cd ~
+
+#clear screen on exit
+clear
+
+#show menu
+menu
